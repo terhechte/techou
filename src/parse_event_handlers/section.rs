0650117cd3eb2d5ab0a2ffb6364090bc7123ef72
Benedikt Terhechte <benedikt.terhechte@xing.com>
2019-01-31 09:00:35 +0100

    Support for linking to sections

--- src/parse_event_handlers/section.rs
+++ src/parse_event_handlers/section.rs
@@ -1,13 +1,16 @@
 use super::*;
+use std::borrow::Cow;
 
 pub struct SectionEventHandler {
-    next_text_is_section: bool
+    next_text_is_section: bool,
+    current_header: String,
 }
 
 impl SectionEventHandler {
     pub fn new() -> SectionEventHandler {
         SectionEventHandler {
-            next_text_is_section: false
+            next_text_is_section: false,
+            current_header: String::new()
         }
     }
 }
@@ -20,8 +23,15 @@ impl EventHandler for SectionEventHandler {
                 self.next_text_is_section = true;
             }
             &Event::Text(ref text) if self.next_text_is_section => {
-                result.sections.push(((result.sections.len() as i32) + 1, text.to_string()));
+                self.current_header.push_str(&text);
+            }
+            &Event::End(Tag::Header(_)) => {
                 self.next_text_is_section = false;
+                let header_number = (result.sections.len() as i32) + 1;
+                let text = std::mem::replace(&mut self.current_header, String::new());
+                result.sections.push((header_number, text));
+                // we insert a small identifier so that the header can be linked to
+                events.push(Event::Html(Cow::Owned(format!("<span id=\"header-section-{}\"></span>", header_number))));
             }
             _ => ()
         }
